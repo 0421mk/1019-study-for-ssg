3e8fbce22c241ff8df22ae6dd6354a2834a3ede7
youngbamer <0421mk@naver.com>
2021-10-19 12:45:13 +0900

    기능별 분기 구현

--- src/com/ssg/Main.java
+++ src/com/ssg/Main.java
@@ -6,13 +6,27 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("==== 프로그램 시작 ====");
-		
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.printf("명령어를 입력해주세요 : ");
-		String command = scanner.nextLine();
+		while(true) {
+			System.out.printf("명령어를 입력해주세요 : ");
+			String command = scanner.nextLine();
+			
+			if(command.equals("article write")) {
+				System.out.println("article write");
+			} else if(command.equals("article list")) {
+				System.out.println("article list");
+			} else if(command.equals("article detail")) {
+				System.out.println("article detail");
+			} else if(command.equals("article modify")) {
+				System.out.println("article modify");
+			} else if(command.equals("system exit")) {
+				break;
+			} else {
+				System.out.println("존재하지 않는 명령어입니다.");
+			}
+		}
 		
-		System.out.println(command);
 		System.out.println("==== 프로그램 끝 ====");
 	}
 
