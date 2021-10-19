8e5390793d388a8a21f281f16ead05d2d7da0bd5
youngbamer <0421mk@naver.com>
2021-10-19 12:39:25 +0900

    입출력 프로그램 구현

--- src/com/ssg/Main.java
+++ src/com/ssg/Main.java
@@ -1,9 +1,19 @@
 package com.ssg;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("안녕");
+		System.out.println("==== 프로그램 시작 ====");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.printf("명령어를 입력해주세요 : ");
+		String command = scanner.nextLine();
+		
+		System.out.println(command);
+		System.out.println("==== 프로그램 끝 ====");
 	}
 
 }
